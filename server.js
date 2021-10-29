a1794c71ff496755345687923be48d3a3bd395a9
Jaffer Abbas <60713786+JafferAbbas-codes@users.noreply.github.com>
2021-10-29 12:58:34 +0500

    heroku push

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-//Install express server
-const express = require("express");
 const path = require("path");
-
+const express = require("express");
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static("./dist/angular-tutorial"));
-
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/angular-tutorial/" })
-);
-
+app.use(express.static(__dirname + "/dist/angular-tutorial/"));
+app.get("/*", function (req, res) {
+  console.log("req",__dirname);
+  res.sendFile(path.join(__dirname, "/dist/angular-tutorial/", "index.html"));
+});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080, () => console.log("server running"));
+
+
